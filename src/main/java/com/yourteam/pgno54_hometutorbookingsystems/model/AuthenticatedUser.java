01681f901d71c1b1fe1d3895f6f3d19947348848
IT24101466 <it24101466@my.sliit.lk>
2025-05-16 12:22:28 +0530

    Updated AuthenticatedUser

--- src/main/java/com/yourteam/pgno54_hometutorbookingsystems/model/AuthenticatedUser.java
+++ src/main/java/com/yourteam/pgno54_hometutorbookingsystems/model/AuthenticatedUser.java
@@ -1,4 +1,28 @@
 package com.yourteam.pgno54_hometutorbookingsystems.model;
 
-public class AuthenticatedUser {
+public abstract class AuthenticatedUser extends User {
+    private String username;
+    private String password;
+
+    public AuthenticatedUser(String name, String username, String password) {
+        super(name);
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
